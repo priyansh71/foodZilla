588c1d26e54ff8bfbdcf949b6023a98dc7516b2e
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-08-19 02:53:10 +0530

    added dependencies and env

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from "react";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import RestaurantStack from "./routes/RestaurantStack";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    let [fontsLoaded] = useFonts({
+        regular: require("./assets/fonts/regular.ttf"),
+    });
+
+      if (!fontsLoaded) {
+        return <AppLoading />;
+      } else {
+    return <RestaurantStack />;
+      }
+};
+
+export default App;
